ff927c26c4c1a0d731f55b2f81721fb06121e616
Rik Harink <rikharink@telluric.dev>
2021-09-02 19:45:04 +0200

    keybinds temp add

--- src/index.ts
+++ src/index.ts
@@ -13,32 +13,16 @@ window.addEventListener('unload', () => {
   Game.dehydrate();
 });
 
-if (DEBUG) {
-  const w = <any>window;
-  w.recorder = () =>
-    new CanvasRecorder(<HTMLCanvasElement>document.getElementById('g'));
-  window.addEventListener('keypress', (e: KeyboardEvent) => {
-    if (e.key === 'r') {
-      Game.resetLevel();
-    } else if (e.key === 'p') {
-      Game.isActive ? Game.stop() : Game.start();
-    } else if (e.key === 'o') {
-      if (!w.recording) {
-        w.recording = true;
-        w.vcr = w.recorder();
-        w.vcr.startRecording();
-      } else if (w.recording) {
-        w.recording = false;
-        w.vcr.stopRecording();
-        w.vcr.download('IPTS-demo.webm');
-        delete w.vcr;
-      }
-    } else if (e.key === 'n') {
-      Game.nextLevel();
-    } else if (e.key === 'k') {
-      Game.reset();
-    } else if (e.key === 'd') {
-      Game.dumpLevel();
-    }
-  });
-}
+window.addEventListener('keypress', (e: KeyboardEvent) => {
+  if (e.key === 'r') {
+    Game.resetLevel();
+  } else if (e.key === 'p') {
+    Game.isActive ? Game.stop() : Game.start();
+  } else if (e.key === 'n') {
+    Game.nextLevel();
+  } else if (e.key === 'k') {
+    Game.reset();
+  } else if (e.key === 'd') {
+    Game.dumpLevel();
+  }
+});
